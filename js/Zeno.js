3dcf8d4a3b281f5aebf1233a7d21c6f5d93f2f1b
pippinbarr <pippin.barr@gmail.com>
2024-06-27 16:33:26 -0400

    Zeno... finished?
    - I'm not 1000% on this, but I feel like the current solution sort of works visually... albeit with a kind of visual cheat... and a bit of weird happenstance
    - So I halve the tile-size for movement each time the snake gets halfway from its position to the apple
    - And I secretly target the tile *before* the apple because that way you see the apple the whole time
    - And for SOME REASON I don't 100% get but is doubtless to do with float accuracy in JS the snake effectively stops right in front of the apple, never getting there... I think there's a point of preceision where you are trying to add the grid size but it's so small it gets rounded away when added to the position... which might be something about a difference between the "normal" float representation (0.327472389423 etc.) and the e representation (1.2e3247237894 etc.)?
    - I don't know, but I quite like it and I like the ways in which it gestures at a version of a world where Zeno is ACTUALLY RIGHT in a weird way...
    - On to the menu

--- js/Zeno.js
+++ js/Zeno.js
@@ -10,18 +10,11 @@ class Zeno extends Snake {
         this.APPLE_START_X = 21;
         this.APPLE_START_Y = 16;
 
-        this.CONTROLS_X = 11;
+        this.CONTROLS_X = 2;
         this.CONTROLS_Y = 18;
 
         this.STARTING_SNAKE_TICK = this.SNAKE_TICK;
-
-        this.halfways = [
-            11,
-            16,
-            18,
-            20
-        ];
-        this.target = 0;
+        this.STARTING_GRID_SIZE = this.GRID_SIZE;
     }
 
     create() {
@@ -35,14 +28,24 @@ class Zeno extends Snake {
         });
 
         this.repositionApple(this.apple);
+
+        const snakeX = this.SNAKE_START_X * this.GRID_SIZE;
+        const appleX = (this.APPLE_START_X - 1) * this.GRID_SIZE;
+        this.halfway = this.getHalfWay(snakeX, appleX);
+        console.log(this.halfway);
     }
 
     tick() {
         super.tick();
 
-        if (this.snakeMoving() && !this.dead) {
-            this.SNAKE_TICK *= 2;
-            console.log(this.SNAKE_TICK);
+        if (this.snakeMoving() && !this.dead && this.snakeHead.x >= this.halfway) {
+            this.GRID_SIZE /= 2;
+            if (this.next.x > 0) {
+                this.next.x = this.GRID_SIZE;
+            }
+            const snakeX = this.snakeHead.x;
+            const appleX = this.apple.x - this.STARTING_GRID_SIZE;
+            this.halfway = this.getHalfWay(snakeX, appleX);
         } else {
             this.SNAKE_TICK = this.STARTING_SNAKE_TICK;
         }
@@ -54,8 +57,29 @@ class Zeno extends Snake {
         });
     }
 
+    getHalfWay(a, b) {
+        return (b - a) / 2 + a;
+    }
+
     snakeMoving() {
         return (this.next.x !== 0 || this.next.y !== 0);
+    }
+
+    updateSnakePosition() {
+        if (this.next.x == 0 && this.next.y == 0) {
+            return;
+        }
+
+        this.moveSFX.play();
+
+        this.snakeHead.x += this.next.x;
+        this.snakeHead.y += this.next.y;
+
+        for (let i = this.snake.length - 2; i >= 0; i--) {
+            this.snake[i].x = this.snake[i + 1].x - this.STARTING_GRID_SIZE;
+            this.snake[i].y = this.snake[i + 1].y;
+        }
+
     }
 
     repositionApple(apple) {
@@ -70,6 +94,8 @@ class Zeno extends Snake {
     }
 
     gameOver() {
+        this.GRID_SIZE = this.STARTING_GRID_SIZE;
+        this.target = 0;
         this.dead = false;
         this.snakeHead.setVisible(true);
         this.snakeBodyGroup.clear(true, true);
@@ -128,6 +154,41 @@ class Zeno extends Snake {
                 }
             }
         }
+    }
+
+    /**
+     * Controls
+     */
+
+    createControls() {
+        let controlsStrings = [];
+        if (this.sys.game.device.os.desktop) {
+            controlsStrings = ["RIGHT", "ARROW", "CONTROLS"];
+        }
+        else {
+            controlsStrings = ["RIGHT", "SWIPE", "CONTROLS"];
+        }
+        controlsStrings.push(this.strings[this.stateName].hero);
+
+        this.addTextToGrid(this.CONTROLS_X, this.CONTROLS_Y, controlsStrings, this.controlsGroup);
+        this.controlsVisible = true;
+    }
+
+    /**
+ * Snake handling (haha)
+ */
+
+    left() {
+    }
+
+    right() {
+        if (this.prev.x == 0) this.next = new Phaser.Geom.Point(this.GRID_SIZE, 0);
+    }
+
+    up() {
+    }
+
+    down() {
     }
 
     /**
