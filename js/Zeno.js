0f6c99318556f288f42265ce2542b153ce66fdd9
pippinbarr <pippin.barr@gmail.com>
2024-06-27 15:34:33 -0400

    Zeno is "half-way"
    - Oh how terribly funny
    - I implemented a version where the tick time doubles every tick and that is visually very funny because the snake takes progressively longer and longer to move one square
    - But then I realized that isn't really much of a reflection of Zeno's paradox and so I stopped laughing and felt a bit sad
    - I start converting it so the tick time would double every time you got "half-way" to the apple, but that won't make a very good point because in that case you'll get there in not much time
    - Because one of my observations I thought was fun was just to *let the snake get the apple* in this version, because the nature of the world almost doesn't *have* Zeno's paradox? But it won't be funny if it happens too fast... there needs to be the "will it happen?" moment first
    - So now I'm a little bit back to the drawing board

--- js/Zeno.js
+++ js/Zeno.js
@@ -4,15 +4,69 @@ class Zeno extends Snake {
             key: "zeno"
         });
 
-        this.SNAKE_START_X = 11;
+        this.SNAKE_START_X = 2;
         this.SNAKE_START_Y = 16;
+
+        this.APPLE_START_X = 21;
+        this.APPLE_START_Y = 16;
 
         this.CONTROLS_X = 11;
         this.CONTROLS_Y = 18;
+
+        this.STARTING_SNAKE_TICK = this.SNAKE_TICK;
+
+        this.halfways = [
+            11,
+            16,
+            18,
+            20
+        ];
+        this.target = 0;
     }
 
     create() {
         super.create();
+
+        this.ticker.remove();
+        this.ticker = this.time.addEvent({
+            callback: this.tick,
+            callbackScope: this,
+            delay: 1000 * this.SNAKE_TICK,
+        });
+
+        this.repositionApple(this.apple);
+    }
+
+    tick() {
+        super.tick();
+
+        if (this.snakeMoving() && !this.dead) {
+            this.SNAKE_TICK *= 2;
+            console.log(this.SNAKE_TICK);
+        } else {
+            this.SNAKE_TICK = this.STARTING_SNAKE_TICK;
+        }
+
+        this.ticker = this.time.addEvent({
+            callback: this.tick,
+            callbackScope: this,
+            delay: 1000 * this.SNAKE_TICK,
+        });
+    }
+
+    snakeMoving() {
+        return (this.next.x !== 0 || this.next.y !== 0);
+    }
+
+    repositionApple(apple) {
+        apple.setVisible(true);
+        apple.x = this.APPLE_START_X * this.GRID_SIZE;
+        apple.y = this.APPLE_START_Y * this.GRID_SIZE;
+        return true;
+    }
+
+    startAppleTimer() {
+
     }
 
     gameOver() {
@@ -29,7 +83,52 @@ class Zeno extends Snake {
      * Create functions
      */
 
+    createWalls() {
+        // Create the walls
+        this.WALL_LEFT = 1;
+        this.WALL_RIGHT = this.NUM_COLS - 2;
+        this.WALL_TOP = 3;
+        this.WALL_BOTTOM = this.NUM_ROWS - this.WALL_TOP - 1;
 
+        let WALLS = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+        this.wallGroup = this.physics.add.group();
+        for (let y = 0; y < WALLS.length; y++) {
+            for (let x = 0; x < WALLS[y].length; x++) {
+                if (WALLS[y][x]) {
+                    let wall = this.wallGroup.create((this.WALL_LEFT + x) * this.GRID_SIZE, (this.WALL_TOP + y) * this.GRID_SIZE, 'wall')
+                    wall.setOrigin(0, 0);
+                }
+            }
+        }
+    }
 
     /**
      * Disabling the exits!
